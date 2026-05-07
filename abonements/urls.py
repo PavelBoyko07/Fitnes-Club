9853c972174e39b48feda951bfd6e900beadc98a
PavelBoyko07 <pbojko67@gmail.com>
2026-05-07 13:49:46 +0300

    полностью отктил все переделал папки и структуру заполнил models.py заполнил views.py заполнил urls.py

--- abonements/urls.py
+++ abonements/urls.py
@@ -1,18 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from abonements import views
-from views import *
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', views.category_list, name='category_list'),
-    path('categories/create/', views.CategoryCreateView.as_view(), name='category_create'),
-    path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
-    path('categories/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category_update'),
-    path('categories/<int:pk>/', views.category_detail, name='category_detail'),
-    path('abonements/', views.AbonementListView.as_view(), name='abonement_list'),
-    path('abonements/create/', views.AbonementCreateView.as_view(), name='abonement_create'),
-    path('abonements/<int:pk>/update/', views.AbonementUpdateView.as_view(), name='abonement_update'),
-    path('abonements/<int:pk>/delete/', views.AbonementDeleteView.as_view(), name='abonement_delete'),
-    path('abonements/<slug:slug>/', views.AbonementDetailView.as_view(), name='abonement_detail'),
+    path('', AbonementListView.as_view(), name='abonement_list'),
+    path('create/', AbonementCreateView.as_view(), name='abonement_create'),
+    path('<int:pk>/', AbonementDetailView.as_view(), name='abonement_detail'),
+    path('<int:pk>/update/', AbonementUpdateView.as_view(), name='abonement_update'),
+    path('<int:pk>/delete/', AbonementDeleteView.as_view(), name='abonement_delete'),
 ]
